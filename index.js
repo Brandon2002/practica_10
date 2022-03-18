61e21143fcb6671ddb877ddd1442bdfcfdf43cbe
Brandon Tapia <btapia@ucol.mx>
2022-03-18 15:29:36 -0600

    comentarios

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require('express'); //importamos la dependencia
 var app = express(); //Declaramos una app de express
 
-var port = process.env.PORT || 3000; //seteamos el puerto para que escuche el servidor
+var port = process.env.PORT || 3000; //seteamos el puerto para que escuche el servidor //indica el puerto por donde se ejecutará el server
 
-//primera ruta(está añ nivel de la raíz /), Hello World!
+//primera ruta(está al nivel de la raíz /), Hello World!
 app.get('/', function(req, res){
-    res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+    res.send('<html><head></head><body><h1>Hello world!</h1></body></html>'); //imprime en html 
 });
 
-app.listen(port); //levantar el server y ponerlo a escuchar.
+app.listen(port); //levantar el server y ponerlo a escuchar. //Asignarle al server a que puerto debe escuchar
 
 app.get('/api', function (req, res){
-    res.json({firstname: 'John', lastname: 'Doe'});
+    res.json({firstname: 'John', lastname: 'Doe'}); //Envía información al server de JSON
 });
 
 app.get('/person/:id', function (req, res){
